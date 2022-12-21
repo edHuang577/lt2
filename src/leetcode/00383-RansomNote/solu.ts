99551133859457ef234fb999eea8d313b30e9aeb
edHuang577 <112370856+edHuang577@users.noreply.github.com>
2022-12-21 16:59:54 +0800

    3rd complete

--- src/leetcode/00383-RansomNote/solu.ts
+++ src/leetcode/00383-RansomNote/solu.ts
@@ -1,7 +1,27 @@
 type func = (a: string, b: string) => boolean;
 const canConstruct: func = (strA, strB) => {
-  let resultNumber = 0;
+  let data: string[] = strB.split("");
 
-  return true;
+  console.log(data);
+  let replacedStr = "";
+  for (let idx = 0; idx < strA.length; idx++) {
+    console.log(strA.charAt(idx));
+    let tmpChar = strA.charAt(idx);
+    for (let idxB = 0; idxB < data.length; idxB++) {
+      const elm = data[idxB];
+      if (tmpChar === elm) {
+        data.splice(idxB, 1);
+        replacedStr += tmpChar;
+        break;
+      }
+    }
+  }
+  console.log(replacedStr);
+  console.log(strA);
+  if (replacedStr === strA) {
+    return true;
+  } else {
+    return false;
+  }
 };
 export default canConstruct;
---
+++ src/test/leetcode/03-00383.test.ts
@@ -0,0 +1,14 @@
+import RansomNote from "leetcode/00383-RansomNote/solu";
+
+let result = RansomNote("aa", "aab");
+console.log(result);
+
+//efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj
+
+let r2 = RansomNote(
+  "bg",
+  "efjbdfbdgfjhhaiigfhbaejahgfbbgbjagbddfgdiaigdadhcfcj"
+);
+console.log(r2);
+
+//so bad....
