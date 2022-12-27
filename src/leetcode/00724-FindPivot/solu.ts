e401f5cc5ec0c03f8f9b2b70b2f3a5f0ee6b5d36
edHuang577 <112370856+edHuang577@users.noreply.github.com>
2022-12-27 17:03:06 +0800

    00724 pass

--- src/leetcode/00724-FindPivot/solu.ts
+++ src/leetcode/00724-FindPivot/solu.ts
@@ -1,13 +1,29 @@
 type func = (a: number[]) => number;
 const pivotIndex: func = (nums) => {
   let resultNumbers: number;
-  resultNumbers = 0;
-
+  resultNumbers = -1;
+  let leftSum = 0;
+  let rightSum = 0;
   for (let i = 0; i < nums.length; i++) {
-    let tmpSum = 0;
-    for (let j = 0; j <= i; j++) {
-      tmpSum += nums[j];
+    for (let j = 0; j < nums.length; j++) {
+      if (j < i) {
+        leftSum += nums[j];
+      } else if (j > i) {
+        rightSum += nums[j];
+      }
     }
+    console.log("====" + i);
+    console.log(leftSum);
+    console.log(rightSum);
+    if (leftSum === rightSum) {
+      console.log("!!!!!!");
+      console.log(i);
+      resultNumbers = i;
+      break;
+    }
+
+    leftSum = 0;
+    rightSum = 0;
   }
 
   return resultNumbers;
---
+++ src/test/stage02-LeetCode75/02-00724.test.ts
@@ -0,0 +1,12 @@
+import func from "leetcode/00724-FindPivot/solu";
+
+let nums = [1, 7, 3, 6, 5, 6];
+// let r1 = func(nums);
+
+let nums1 = [1, 2, 3];
+let r2 = func(nums1);
+
+let nums2 = [2, 1, -1];
+// let r3 = func(nums2);
+console.log("result:");
+console.log(r2);
